71fcda26297e998a80aa7e6b5a2c759ef4f3d0b9
nawabgit <nawab.mir14@gmail.com>
2019-12-04 17:49:41 +0000

    added Search class and dfs method

---
+++ src/search.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Search:
+    """
+    The class containing all search algorithms that the agent utilises
+    """
+
+    def dfs(start_state, end_state):
+        t_complexity = 0
+        initial_node = Node(start_state)
+        stack = deque()
+        stack.push(initial_node)
+        while stack:
+            node = stack.pop()
+            if node.is_end_state(end_state):
+                return [node, t_complexity]
+
+            for n in node.generate_children():
+                stack.push(n)
+                t_complexity +=1
+
+        raise Exception("No solution found!")
+
+    def bfs()
